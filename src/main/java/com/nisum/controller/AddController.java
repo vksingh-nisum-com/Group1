9725341a63eef5803b793f1b4ac56fd1b19fa25a
Vikram <vksingh@nisum.com>
2018-09-12 13:59:06 +0530

    initial upload

---
+++ src/main/java/com/nisum/controller/AddController.java
@@ -0,0 +1,29 @@
+package com.nisum.controller;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+@Controller
+public class AddController {
+	
+	@RequestMapping("/add")
+	public void add(HttpServletRequest request,HttpServletResponse response) throws IOException{
+		
+	}
+
+}
