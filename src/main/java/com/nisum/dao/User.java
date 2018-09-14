ea839f2b15526821126cc8607aab9a8a1b6a66ce
lakshmi <vksingh@nisum.com>
2018-09-14 11:41:22 +0530

    Added User object

---
+++ src/main/java/com/nisum/dao/User.java
@@ -0,0 +1,54 @@
+package com.nisum.dao;
+
+public class User {
+	
+	private int id;
+	private String fullName;
+	private String userName;
+	private  String password;
+	private String rePassword;
+	private String email;
+	private String phone;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRePassword() {
+		return rePassword;
+	}
+	public void setRePassword(String rePassword) {
+		this.rePassword = rePassword;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+}
